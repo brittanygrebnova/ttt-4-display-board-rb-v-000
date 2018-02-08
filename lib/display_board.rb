c0649c134cb03631b16ed07f102a38dfa7d6efb2
Brittany Grebnov <brittanygrebnova@gmail.com>
2018-02-08 22:40:36 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 def display_board(board)
-  puts "   | X |   "
-  puts "-----------"
   puts "   |   |   "
+  puts "-----------"
+  puts "   | X |   "
   puts "-----------"
   puts "   |   |   "
 end
